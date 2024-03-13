e4dc6b61313d2c48061e804f6ca9bd20ea848a4b
xtmc <tmc.invest2018@gmail.com>
2024-03-13 04:25:58 +0100

    adjusted5 nextconfig

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 const nextConfig = {
+  mode: 'development',
   reactStrictMode: true,
   swcMinify: true,
   webpack(config, { dev, isServer }) {
     // Use a suitable devtool configuration for development mode
     if (dev) {
-      config.devtool = 'source-map'; // or any other suitable devtool option
+      config.devtool = 'eval-source-map'; // or any other suitable devtool option
     }
 
     // Add any other webpack configurations here if needed
