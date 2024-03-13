e090183a3e74bbdbd7729ba7fc65a1ace50c8b8f
xtmc <tmc.invest2018@gmail.com>
2024-03-13 04:49:37 +0100

    adjusted5 nextconfig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  swcMinify: true, // Remove this line (not needed for disabling minification)
+  webpack: (config, { dev, isServer }) => {
+    // Disable minification in production build
+    if (!dev && !isServer) {
+      config.optimization.minimize = false;
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
