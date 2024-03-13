5c84cc9f4626f9eae300e01f6e0ada4bfc2bb0de
xtmc <tmc.invest2018@gmail.com>
2024-03-13 02:52:36 +0100

    adjusted3 nextconfig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  target: 'serverless', // Assuming you're deploying to a serverless environment
+  env: {
+    NEXT_PUBLIC_ALCHEMY_ID: process.env.NEXT_PUBLIC_ALCHEMY_ID,
+    NEXT_PUBLIC_CONTRACT_ADDRESS: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+    NEXT_PUBLIC_PRESALE_ID: process.env.NEXT_PUBLIC_PRESALE_ID,
+    NEXT_PUBLIC_CHAIN_ID: process.env.NEXT_PUBLIC_CHAIN_ID,
+    NEXT_PUBLIC_CONTRACT_ABI: process.env.NEXT_PUBLIC_CONTRACT_ABI,
+    NEXT_PUBLIC_STABLE_COIN_CONTRACT_ABI: process.env.NEXT_PUBLIC_STABLE_COIN_CONTRACT_ABI,
+  },
+  // Other configurations...
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
