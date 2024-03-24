58e4a94d65b87d6e41f19363ca7c0bf845411363
xtmc <tmc.invest2018@gmail.com>
2024-03-24 05:04:00 +0100

    added refferal/front/back

--- next-i18next.config.js
+++
@@ -1,9 +0,0 @@
-// next-i18next.config.js
-const { i18n } = require('next-i18next');
-
-module.exports = {
-  i18n: {
-    defaultLocale: 'en',
-    locales: ["en","ru","hi","es","ko","tr","fr","nl","zh","ar","de","it","ja","pt"],
-  },
-};
