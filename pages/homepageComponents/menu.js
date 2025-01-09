a0ff7180a9a8da12fd23f01d6f5b3e0563f169a8
tmcinvest2018 <35514856+tmcinvest2018@users.noreply.github.com>
2025-01-09 16:37:45 +0100

    Update menu.js linked aigosswap

--- pages/homepageComponents/menu.js
+++ pages/homepageComponents/menu.js
@@ -1,7 +1,6 @@
 import { Navbar, Button } from "flowbite-react";
 
-export default function Menu()
-{
+export default function Menu() {
     return (
         <>
             {/* Menu START */}
@@ -11,7 +10,7 @@ export default function Menu()
                 className="px-2 sm:px-4 py-2.5 bg-gradient-to-b from-neutral-900 to-neutral-800 fixed w-full z-20 top-0 left-0 border-b border-neutral-200 border-neutral-600"
             >
                 <Navbar.Brand href="#">
-                <img src="/images/logo1.png" style={{ width: '3.5rem' }} className="mr-2" alt="AIGOS LOGO" />
+                    <img src="/images/logo1.png" style={{ width: '3.5rem' }} className="mr-2" alt="AIGOS LOGO" />
                 </Navbar.Brand>
                 <div className="flex md:order-2">
                     <a href="#section4">
@@ -68,10 +67,17 @@ export default function Menu()
                         href="#section9">
                         ROADMAP
                     </Navbar.Link>
+                    <Navbar.Link
+                        className="uppercase block py-2 pr-4 pl-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-700 md:p-0 md:hover:text-white text-gray-400 hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700"
+                        href="https://aigosswap.vercel.app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        AIGOSDEX
+                    </Navbar.Link>
                 </Navbar.Collapse>
             </Navbar>
             {/* Menu END */}
         </>
-    )
+    );
 }
-
